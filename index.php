eb2af991364f3c2915c0dd7e9df1ea3db284ff7e
ovictorqueiroz <victorbr442@gmail.com>
2025-10-21 19:43:16 -0300

    iniciando repo

--- index.php
+++ index.php
@@ -8,7 +8,19 @@
 	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
 </head>
 <body>
-	<h1 class="d-flex justify-content-around">Seja Membro</h1>
+	<nav class="d-flex align-items-center flex-column bg-secondary">
+	<h1>Seja Membro</h1>
 	<p>Tornando o mundo mais bem-informado, uma newsletter por vez.</p>
+	</nav>
+
+	<form action="#" method="post">
+		<label>Primeiro Nome</label>
+		<input type="text" name="primeiroNome" id="primeiroNome" maxlength="50" required>
+
+		<label>Sobrenome</label>
+		<input type="text" name="sobrenome" id="sobrenome" >
+	</form>
+
+	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>	
