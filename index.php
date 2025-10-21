74c9b49571e9762a75043fe5ec639d72f167aa1e
ovictorqueiroz <victorbr442@gmail.com>
2025-10-21 19:31:44 -0300

    iniciando repo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Meu Site em PHP</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<h1 class="d-flex justify-content-around">Seja Membro</h1>
+	<p>Tornando o mundo mais bem-informado, uma newsletter por vez.</p>
+</body>
+</html>	
