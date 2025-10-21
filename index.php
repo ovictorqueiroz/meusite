58e2c6d90d79bd87776952bf7ca9b891b8e868e0
ovictorqueiroz <victorbr442@gmail.com>
2025-10-21 19:51:07 -0300

    formulario

--- index.php
+++ index.php
@@ -13,12 +13,27 @@
 	<p>Tornando o mundo mais bem-informado, uma newsletter por vez.</p>
 	</nav>
 
-	<form action="#" method="post">
-		<label>Primeiro Nome</label>
+	<form action="#" method="post" class="d-flex flex-column">
+		<label for="primeiroNome">Nome</label>
 		<input type="text" name="primeiroNome" id="primeiroNome" maxlength="50" required>
 
-		<label>Sobrenome</label>
+		<label for="sobrenome">Sobrenome</label>
 		<input type="text" name="sobrenome" id="sobrenome" >
+
+		<label for="useremai">Email</label>
+		<input type="emai" name="useremail" id="useremail">
+
+		<label>Escolha sua Newsletter:</label>
+		<select>
+			<option value="noticias">Notícias</option>
+			<option value="esportes">Esportes</option>
+			<option value="negocios">Negócios</option>
+			<option value="cultura">Cultura</option>
+			<option value="cronicas">Crônicas</option>
+			<option value="gastronomia">Gastronomia</option>
+		</select>
+		<button type="submit">Enviar</button>
+		<button type="reset">Limpar</button>
 	</form>
 
 	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
