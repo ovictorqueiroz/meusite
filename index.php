7d6c1d05ddd6f22f5fb7224d08e8a9559366e73d
ovictorqueiroz <victorbr442@gmail.com>
2025-10-21 20:17:39 -0300

    estilizando

--- index.php
+++ index.php
@@ -8,34 +8,52 @@
 	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
 </head>
 <body>
-	<nav class="d-flex align-items-center flex-column bg-secondary">
-	<h1>Seja Membro</h1>
-	<p>Tornando o mundo mais bem-informado, uma newsletter por vez.</p>
+	<nav class="d-flex align-items-center flex-column">
+		<h1>Seja Membro</h1>
+		<p>Tornando o mundo mais bem-informado, uma newsletter por vez.</p>
 	</nav>
+	<main class="container">
+		
 
-	<form action="#" method="post" class="d-flex flex-column">
-		<label for="primeiroNome">Nome</label>
-		<input type="text" name="primeiroNome" id="primeiroNome" maxlength="50" required>
+		<form action="#" method="post">
+			<div class="md-3">
+				<label for="primeiroNome">Nome</label>
+				<input type="text" name="primeiroNome" id="primeiroNome" maxlength="50" required>
+			</div>
+			
+			<div class="md-3">
+				<label for="sobrenome">Sobrenome</label>
+				<input type="text" name="sobrenome" id="sobrenome" >
+			</div>
 
-		<label for="sobrenome">Sobrenome</label>
-		<input type="text" name="sobrenome" id="sobrenome" >
+			
+			<div class="md-3">
+				<label for="useremai">Email</label>
+				<input type="emai" name="useremail" id="useremail">
+			</div>
 
-		<label for="useremai">Email</label>
-		<input type="emai" name="useremail" id="useremail">
+			
+			<div class="md-3">
+				<label>Escolha sua Newsletter:</label>
+				<select>
+					<option value="noticias">Notícias</option>
+					<option value="esportes">Esportes</option>
+					<option value="negocios">Negócios</option>
+					<option value="cultura">Cultura</option>
+					<option value="cronicas">Crônicas</option>
+					<option value="gastronomia">Gastronomia</option>
+				</select>
+			</div>
 
-		<label>Escolha sua Newsletter:</label>
-		<select>
-			<option value="noticias">Notícias</option>
-			<option value="esportes">Esportes</option>
-			<option value="negocios">Negócios</option>
-			<option value="cultura">Cultura</option>
-			<option value="cronicas">Crônicas</option>
-			<option value="gastronomia">Gastronomia</option>
-		</select>
-		<button type="submit">Enviar</button>
-		<button type="reset">Limpar</button>
-	</form>
+			<button type="submit" class="md-3 btn btn-primary">Enviar</button>
 
-	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+			
+			<button type="reset" class="md-3 btn btn-danger">Limpar</button>
+			
+
+		</form>
+	</main>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>	
